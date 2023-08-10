1122233e7e98cd26bea71e93ccfb1c56d0684d07
Ginlon <2675130594@qq.com>
2023-08-10 18:09:09 +0800

    feat(app): map demo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import "ol/ol.css"
+import OlMap from "ol/Map.js"
+import View from "ol/View.js"
+import { BingMaps, TileWMS } from "ol/source"
+import { Tile } from "ol/layer"
+import { onMounted } from "vue"
+import TileLayer from "ol/layer/Tile"
+
+const styles = [
+  "RoadOnDemand",
+  "Aerial",
+  "AerialWithLabelsOnDemand",
+  "CanvasDark",
+  "OrdnanceSurvey",
+]
+const layers: Tile<BingMaps>[] = []
+let i, ii
+for (i = 0, ii = styles.length; i < ii; ++i) {
+  layers.push(
+    new Tile({
+      visible: false,
+      preload: Infinity,
+      source: new BingMaps({
+        // https://www.bingmapsportal.com/Application
+        key: "Your Bing Maps Key",
+        imagerySet: styles[i],
+      }),
+    })
+  )
+}
+
+onMounted(() => {
+  new OlMap({
+    layers: layers,
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+    target: "map1",
+  })
+
+  const select = document.getElementById("layer-select") as HTMLSelectElement
+  select.addEventListener("change", onChange)
+  function onChange() {
+    const style = select.value
+    for (let i = 0, ii = layers.length; i < ii; ++i) {
+      layers[i].setVisible(styles[i] === style)
+    }
+  }
+
+  new OlMap({
+    target: "map2",
+    view: new View({
+      projection: "EPSG:3857",
+      center: [0, 0],
+      zoom: 2,
+    }),
+    layers: [
+      new TileLayer({
+        source: new TileWMS({
+          projection: "EPSG:4326", // 会重新投影到 EPSG:3857
+          url: "https://ahocevar.com/geoserver/wms",
+          params: {
+            LAYERS: "ne:NE1_HR_LC_SR_W_DR",
+          },
+        }),
+      }),
+    ],
+  })
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>OpenLayers</h1>
+  <div class="map" id="map1"></div>
+  <select id="layer-select">
+    <option value="Aerial">Aerial</option>
+    <option value="AerialWithLabelsOnDemand" selected>
+      Aerial with labels
+    </option>
+    <option value="RoadOnDemand">Road</option>
+    <option value="CanvasDark">Road dark</option>
+    <option value="OrdnanceSurvey">Ordnance Survey</option>
+  </select>
+
+  <div class="map" id="map2"></div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+.map {
+  position: relative;
+  width: 80vw;
+  height: 80vh;
 }
 </style>
